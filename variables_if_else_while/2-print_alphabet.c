2fff41276f8c9b0aac4f729d4e46f4a787695d70
MasibongeNsibande <nsibandemasibonge@gmail.com>
2023-02-22 00:46:03 -0800

    Task2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
-/**
- * main - Prints the alphabetic in lower and upper case
- *
- * Return: Always (Success)
- */
-int main(void)
+void main()
+
 {
-	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
+        int ch;
+	clrscr();
 
-	for (c = 'A'; c <= 'Z'; c++)
-	{
-		putchar(c);
-	}
+        printf(“Print all uppercase letters followed by all lowercase letters on the next line :\n”);
 
-	putchar('\n');
+        /* print uppercase letters */
 
-	return (0);
+        for (ch= ‘A’; ch<= ‘Z’; ch++)
+
+             putchar(ch);
+
+             putchar (‘\n’ ) ;
+
+             /* print lowercase letters */
+
+             for (ch= ‘a’; ch<= ‘z’; ch++)
+
+                  putchar(ch);
+
+                  putchar (‘\n’) ;
+
+                  getch();
+
 }
