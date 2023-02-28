efa1b703c57b8e788d8903bbfe28b2f814fe432e
MasibongeNsibande <nsibandemasibonge@gmail.com>
2023-02-28 04:55:18 -0800

    Task0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,8 @@
 
 /**
  *Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
-
- Prototype: void reset_to_98(int *n); 
+ *
+ * Prototype: void reset_to_98(int *n); 
  *
  * 
  */
