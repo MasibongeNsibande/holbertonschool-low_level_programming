e28f510c4c1096834f36b29f2f05932ee6666baa
MasibongeNsibande <nsibandemasibonge@gmail.com>
2023-03-01 05:17:17 -0800

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,32 +1,22 @@
 #include "main.h"
 
 /**
- * _strncpy - a function that copys the strings.
- * @dest: an input string
- * @src: an input string
- * @n: an input integer
- * Return: A pointer to the resulting string
+ * _strchr - locate character in a string
+ * @s: char array string
+ * @c: char to look for
+ * Return: NULL if char not found, or pointer to first occurrence of char `c`
  */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
 
-	while (*src)
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
 	{
-		srclen++;
-		src++;
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
+		s++;
 	}
 
-	srclen++;
-
-	if (n > srclen)
-		n = srclen;
-
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-
-	return (temp);
+	return (s + 1);
 }
